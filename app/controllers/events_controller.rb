c95f85b375db0776e495c930dca2b60e2cf6d6d2
lecontroller <anujgupta@windowslive.com>
2022-11-12 22:36:46 +0700

    add flashes for edit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,11 +17,34 @@ class EventsController < ApplicationController
     @event = current_user.events.build(event_params)
 
     if @event.save
-      flash[:success] = "Event Successfully Saved"
+      flash[:success] = "Event Successfully Saved!"
       redirect_to @event
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      flash[:success] = "Event successfully edited!"
+      redirect_to @event
+    else
+      flash.now[:error] = "Unable to edit event!"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to root_path
   end
 
   private

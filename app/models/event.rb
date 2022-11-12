80eea2cb8a5ebed7832a109fbec58ab94208ed05
lecontroller <anujgupta@windowslive.com>
2022-11-13 01:08:18 +0700

    add attendees and attended_event associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, foreign_key: "creator_id", class_name: "User"
+  has_many :attendances, foreign_key: :attended_event_id
+  has_many :attendees, :through => :attendances, source: :attendee
 
   validates :title, :location, :date, presence: true
 end

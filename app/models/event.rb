940742c5a53ab7451eac537ede801318c77cbd84
lecontroller <anujgupta@windowslive.com>
2022-11-13 15:58:40 +0700

    refactor past and future into scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,8 @@ class Event < ApplicationRecord
   has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, :through => :attendances, source: :attendee
 
+  scope :past, -> { where("date < ?", Date.today) }
+  scope :future, -> { where("date > ?", Date.today) }
+
   validates :title, :location, :date, presence: true
 end

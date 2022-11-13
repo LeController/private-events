cf1e53dfc39b9864a212c7112b475946404f7fae
lecontroller <anujgupta@windowslive.com>
2022-11-13 11:22:03 +0700

    attendances controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,17 @@
 class AttendancesController < ApplicationController
+  def new
+    # @attendance = Attendance.new
+  end
+
+  def create
+    event = Event.find(params[:event_id])
+    attendance = Attendance.new(attended_event_id: event.id, attendee_id: current_user.id)
+
+    if attendance.save
+      flash[:notice] = "Your attendance has been noted!"
+      redirect_to event
+    else
+      flash[:error] = "Event cannot be attended."
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :events
+  resources :attendances
 
   # Defines the root path route ("/")
   root "events#index"
